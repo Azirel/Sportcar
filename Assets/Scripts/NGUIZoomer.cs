ae4823749c47d5444e7831fcc26acca96a18acc4
Azirel <azirel92@gmail.com>
2018-04-20 11:00:45 +0300

    Progress commit;

---
+++ Assets/Scripts/NGUIZoomer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TouchScript.Gestures.TransformGestures;
+using UnityEngine;
+
+public class NGUIZoomer : MonoBehaviour
+{
+
+    [SerializeField] TransformGesture gesture;
+    [SerializeField] UIWidget scaledWidget;
+
+    public int maxWidth = 3333;
+    public float zoomSpeed = 1.5f;
+
+    private float deltaMagnitude = 0;
+    private int minWidth;
+
+    void Start()
+    {
+        gesture.Transformed += Gesture_Transformed;
+        minWidth = scaledWidget.width;
+    }
+
+    private void Gesture_Transformed(object sender, System.EventArgs e)
+    {
+        scaledWidget.width = (int)((float)scaledWidget.width * gesture.DeltaScale);
+        if (scaledWidget.width > maxWidth)
+        {
+            scaledWidget.width = maxWidth;
+        }
+        else if (scaledWidget.width < minWidth)
+        {
+            scaledWidget.width = minWidth;
+        }
+    }
+}
